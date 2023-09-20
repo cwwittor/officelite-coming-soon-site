2aca68c36bb7a0c5e556ee99c83597e6f1a23b45
Christian Wittorp <christian@swarminteractive.com>
2023-09-20 10:57:42 -0400

    feat: add js date find starter

---
+++ src/script.js
@@ -0,0 +1,11 @@
+let currentSeconds = 0;
+let currentMinutes = 0;
+let currentHours = 0;
+let currentDays = 0;
+
+let currentDateString = "";
+
+function getDate() {
+  console.log(Date.now());
+  return Date.now();
+}
