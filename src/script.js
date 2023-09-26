2a1896e50c486968609aaefbc8faf99208961542
Christian Wittorp <christian@swarminteractive.com>
2023-09-26 10:00:40 -0400

    feat: add js functionality

--- src/script.js
+++ src/script.js
@@ -5,7 +5,45 @@ let currentDays = 0;
 
 let currentDateString = "";
 
-function getDate() {
-  console.log(Date.now());
-  return Date.now();
+const months = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
+];
+
+function setDeadline() {
+  const d = new Date("2023-12-25 12:00:00");
+  // document.getElementById("deadlineDate").innerHTML = `${d
+  //   .getDate()
+  //   .format("DD MMM YYYY")} ${d.getMonth()} ${d.getFullYear()}`;
+  document.getElementById("deadlineDate").innerHTML = "25 Dec 2023";
 }
+
+function getDate() {
+  const d = new Date("2023-12-25 12:00:00");
+  const today = new Date();
+
+  let timeDifference = d.getTime() - today.getTime();
+
+  var day = Math.floor(timeDifference / (1000 * 3600 * 24));
+  var hour = Math.floor((timeDifference % (1000 * 3600 * 24)) / (1000 * 3600));
+  var minute = Math.floor((timeDifference % (1000 * 3600)) / (1000 * 60));
+  var second = Math.floor((timeDifference % (1000 * 60)) / 1000);
+
+  document.getElementById("days").innerHTML = day;
+  document.getElementById("hours").innerHTML = hour;
+  document.getElementById("minutes").innerHTML = minute;
+  document.getElementById("seconds").innerHTML = second;
+}
+
+//UNCOMMENT THIS TO MAKE TIME WORK
+// setInterval(getDate, 1000);
